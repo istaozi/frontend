718b32b52d9e72b16f6a47929a0192895201ce44
liuxingxing <lxx_0507@163.com>
2016-09-30 18:56:37 +0800

    ssdfsdf

--- xinxipilou-liuxingxing-to-toname-20160929/res/index.js
+++ xinxipilou-liuxingxing-to-toname-20160929/res/index.js
@@ -20,4 +20,65 @@ $(function () {
             $(this).addClass("current_page").siblings().removeClass("current_page");
         });
     });
+
+   /*季度成交额*/
+    var dataChartC = echarts.init(document.getElementById('jdcje'));
+    var optiona = {
+        tooltip: {
+            show: true,
+            formatter: "{b}xx : {c}"
+        },
+        calculable: true,
+        xAxis: [
+            {
+                type: 'category',
+                boundaryGap: false,
+                splitLine:false,//隐藏竖着的分割线
+                axisLine:false,//隐藏坐标线
+                axisLabel : { //x轴文字颜色
+                    show : true,
+                    textStyle : {
+                        fontSize: 16,
+                        fontWeight: 'normal',
+                        fontFamily: 'Arial',
+                        color : '#3f9bff'
+                    }
+                } ,
+                data: ['2015Q1', '2015Q3', '2015Q6', '2015Q9', '2014Q12', '2016Q1', '2016Q3', '2016Q6']
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value',
+                axisLabel: {
+                    show: true,
+                    textStyle: {  //y轴文字颜色
+                        fontSize: 16,
+                        fontWeight: 'normal',
+                        fontFamily: 'Arial',
+                        color: '#666'
+                    }
+                },
+                axisLine:false//隐藏坐标线
+            }
+        ],
+        series: [
+            {
+                name: 'yingli',
+                type: 'line',
+                itemStyle: {
+                    normal: {},
+                    emphasis: {}
+                },
+                /*symbol:"res/img/plot.png",
+                symbolSize: 8,*/
+                symbol: 'arrow',
+                symbolSize: 6,
+                symbolRotate: -45,
+                data: [1, 2, 4, 9, 10, 9, 8, 10, 12, 15]
+            }
+        ],
+        grid: {borderWidth: 0}//隐藏grid边框线
+    }
+    dataChartC.setOption(optiona);
 });
